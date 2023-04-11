cdeb957365fd4d69596250606fc39818a8433740
Abiola David <davidolanrewaju2000@gmail.com>
2023-04-11 16:07:13 +0100

    Set up database entitities

---
+++ app/src/main/java/com/sirdave/composeapp/data/mapper/CompanyMapper.kt
@@ -0,0 +1,20 @@
+package com.sirdave.composeapp.data.mapper
+
+import com.sirdave.composeapp.data.local.CompanyListingEntity
+import com.sirdave.composeapp.domain.model.CompanyListing
+
+fun CompanyListingEntity.toCompanyListing(): CompanyListing {
+    return CompanyListing(
+        name = name,
+        symbol = symbol,
+        exchange = exchange
+    )
+}
+
+fun CompanyListing.toCompanyListingEntity(): CompanyListingEntity {
+    return CompanyListingEntity(
+        name = name,
+        symbol = symbol,
+        exchange = exchange
+    )
+}
