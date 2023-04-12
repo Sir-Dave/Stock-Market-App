584cb6f6d0e01dc934ec433ac1d030f90fa6e159
Abiola David <davidolanrewaju2000@gmail.com>
2023-04-12 17:52:07 +0100

    Data shows in screen

--- app/src/main/java/com/sirdave/composeapp/MainActivity.kt
+++ app/src/main/java/com/sirdave/composeapp/MainActivity.kt
@@ -3,17 +3,24 @@ package com.sirdave.composeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import com.ramcosta.composedestinations.DestinationsNavHost
+import com.sirdave.composeapp.presentation.company_listings.NavGraphs
 import com.sirdave.composeapp.ui.theme.ComposeAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeAppTheme {
-                Surface(modifier = Modifier.fillMaxSize()) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    DestinationsNavHost(navGraph = NavGraphs.root)
                 }
             }
         }
