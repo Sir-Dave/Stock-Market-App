9a848b134b0eb0bb5fae3c25d1411cca4d521ded
Abiola David <davidolanrewaju2000@gmail.com>
2023-04-12 12:48:06 +0100

    Set default parameter for API key

--- app/src/main/java/com/sirdave/composeapp/data/remote/StockApi.kt
+++ app/src/main/java/com/sirdave/composeapp/data/remote/StockApi.kt
@@ -8,7 +8,7 @@ interface StockApi {
 
     @GET("/query?function=LISTING_STATUS")
     suspend fun getListings(
-        @Query("apikey") apiKey: String) : ResponseBody
+        @Query("apikey") apiKey: String = API_KEY) : ResponseBody
 
     companion object{
         const val BASE_URL = "https://alphavantage.co"
