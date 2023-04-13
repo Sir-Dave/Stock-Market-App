27b35f9a706067f7090a36469c7e037fdd66705f
Abiola David <davidolanrewaju2000@gmail.com>
2023-04-13 13:39:56 +0100

    Created domain model for CompanyInfo

---
+++ app/src/main/java/com/sirdave/composeapp/domain/model/CompanyInfo.kt
@@ -0,0 +1,9 @@
+package com.sirdave.composeapp.domain.model
+
+data class CompanyInfo(
+    val name: String,
+    val symbol: String,
+    val description: String,
+    val industry: String,
+    val country: String
+)
