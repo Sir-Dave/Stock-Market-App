0b53e5aa92e6636d7fa9563fa9300ff9a362bc16
Abiola David <davidolanrewaju2000@gmail.com>
2023-04-13 13:46:22 +0100

    Implemented mapper for IntradayInfo

---
+++ app/src/main/java/com/sirdave/composeapp/data/mapper/IntradayInfoMapper.kt
@@ -0,0 +1,17 @@
+package com.sirdave.composeapp.data.mapper
+
+import com.sirdave.composeapp.data.remote.dto.IntradayInfoDto
+import com.sirdave.composeapp.domain.model.IntradayInfo
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+fun IntradayInfoDto.toIntradayInfo(): IntradayInfo{
+    val pattern = "yyyy-MM-dd HH:mm:ss"
+    val formatter = DateTimeFormatter.ofPattern(pattern, Locale.getDefault())
+    val localDateTime = LocalDateTime.parse(timestamp, formatter)
+    return IntradayInfo(
+        date = localDateTime,
+        close = close
+    )
+}
