a16387458e0204764fd6a66cba5c97726f05c67b
Abiola David <davidolanrewaju2000@gmail.com>
2023-04-18 17:47:05 +0100

    Created mappers between IntradayInfo domain model and entity

--- app/src/main/java/com/sirdave/composeapp/data/mapper/IntradayInfoMapper.kt
+++ app/src/main/java/com/sirdave/composeapp/data/mapper/IntradayInfoMapper.kt
@@ -1,5 +1,6 @@
 package com.sirdave.composeapp.data.mapper
 
+import com.sirdave.composeapp.data.local.IntradayInfoEntity
 import com.sirdave.composeapp.data.remote.dto.IntradayInfoDto
 import com.sirdave.composeapp.domain.model.IntradayInfo
 import java.time.LocalDateTime
@@ -7,6 +8,28 @@ import java.time.format.DateTimeFormatter
 import java.util.*
 
 fun IntradayInfoDto.toIntradayInfo(): IntradayInfo{
+    val pattern = "yyyy-MM-dd HH:mm:ss"
+    val formatter = DateTimeFormatter.ofPattern(pattern, Locale.getDefault())
+    val localDateTime = LocalDateTime.parse(timestamp, formatter)
+    return IntradayInfo(
+        date = localDateTime,
+        close = close
+    )
+}
+
+fun IntradayInfo.toIntradayInfoEntity(symbol: String): IntradayInfoEntity{
+    val pattern = "yyyy-MM-dd HH:mm:ss"
+    val formatter = DateTimeFormatter.ofPattern(pattern, Locale.getDefault())
+    val formattedDateTime = date.format(formatter)
+
+    return IntradayInfoEntity(
+        timestamp = formattedDateTime,
+        close = close,
+        companySymbol = symbol
+    )
+}
+
+fun IntradayInfoEntity.toIntradayInfo(): IntradayInfo{
     val pattern = "yyyy-MM-dd HH:mm:ss"
     val formatter = DateTimeFormatter.ofPattern(pattern, Locale.getDefault())
     val localDateTime = LocalDateTime.parse(timestamp, formatter)
